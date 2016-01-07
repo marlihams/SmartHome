bb6d020c7e9e48e4261adcf2aa7fcd4de8d9fda5
Mdiallo <mohamedmadioud@yahoo.fr>
2016-01-07 23:22:57 +0100

    adding smartHomeControllerI

---
+++ app/src/main/java/com/smarthome/controller/SmartHomeControllerI.java
@@ -0,0 +1,11 @@
+package com.smarthome.controller;
+
+import com.smarthome.view.SmartView;
+
+/**
+ * Created by Mdiallo on 27/12/2015.
+ */
+public interface SmartHomeControllerI {
+    public SmartView getView();
+
+}
